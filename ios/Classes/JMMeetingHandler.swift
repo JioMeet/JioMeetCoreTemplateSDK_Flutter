087093ce5b0c072b35dece324c5dc93ddf147e5f
Shivam Tripathi <shivam.tripathi@ril.com>
2025-07-31 10:44:21 +0530

    added config for speaker restrictions and minimize maximize meeting view

--- ios/Classes/JMMeetingHandler.swift
+++ ios/Classes/JMMeetingHandler.swift
@@ -145,6 +145,7 @@ class JMMeetingHandler: NSObject {
         if let isSpotlightFeatureEnabled = config["isSpotlightFeatureEnabled"] as? Bool {
             JMUIKit.isSpotlightFeatureEnabled = isSpotlightFeatureEnabled
         }
+        JMUIKit.isAudioFromExternalSource = true
         JMUIKit.showThankYouScreen = false
     }
     
@@ -160,6 +161,14 @@ class JMMeetingHandler: NSObject {
     func removeJioMeetView(){
         DispatchQueue.main.async {
             self.jioMeetView.removeFromSuperview()
+        }
+    }
+    
+    func maximizeMeetingView() {
+        // Show top and bottom controls for the meeting view
+        // This is equivalent to Android's showTopBottomControls()
+        DispatchQueue.main.async {
+            self.jioMeetView.maximizeMeetingView() //showTopBottomControls()
         }
     }
     
@@ -241,6 +250,12 @@ class JMMeetingHandler: NSObject {
 extension JMMeetingHandler: JMMeetingViewDelegate {
     func didLocalUserJoinsMeeting() {
         JioCoreSdkPlugin.eventSink?("meetingStarted")
+    }
+    
+    func minimizeMeetingView() {
+        // This callback is triggered when the meeting view is minimized
+        // Equivalent to Android's minimizeMeetingView() callback
+        JioCoreSdkPlugin.eventSink?("minimizeMeetingView")
     }
         
     func didLocalUserExitsMeetingView() {
